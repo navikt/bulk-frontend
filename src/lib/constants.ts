2e54a46c6f23b19d0f34d631614abc0a0f595226
Ulrik Røsby <ulrik@rosby.no>
2022-06-29 15:22:47 +0200

    fix: environement loading errors
    Co-authored-by: ahmeds99 <ahmed99@live.com>
    Co-authored-by: sigbbe <sigbjorn.berdal14@gmail.com>

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const BACKEND_URL =
-  process.env.NODE_ENV === "development"
+  process.env.NODE_ENV === "production"
     ? process.env.NEXT_PUBLIC_BACKEND_URL
     : "http://127.0.0.1:8080";
