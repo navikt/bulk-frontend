c8f4c7f7e150cba5250b2b4be087e7f48b23a16a
ahmeds99 <ahmed99@live.no>
2022-07-11 12:13:08 +0200

    Feat: removed previous index.tsx and renamed main.tsx to index.tsx in pages
    Co-authored-by: ulrik2204 <ulrik@rosby@no>
    Co-authored-by: sigbbe <sigbjorn.berdal14@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,30 @@
-import type { NextPage } from "next";
-import { useRouter } from "next/router";
+import { NextPage } from "next/types";
 import { useCallback } from "react";
+import { useQuery } from "react-query";
+import { BACKEND_URL } from "../lib/constants";
+import {getHelloFromAPI, getIsAlive, getPeople} from "../lib/requests"
 
-const Home: NextPage = () => {
-  const router = useRouter();
+const Main: NextPage = () => {
+  const { data, isLoading, isError, error, refetch } = useQuery(
+    "hello-world",
+    getPeople,
+    {
+      enabled: false,
+    }
+  );
 
-  const onLogInClick = useCallback(() => {
-    router.push("/main");
-  }, [router]);
+  const onRequestClick = useCallback(() => {
+    refetch();
+  }, []);
 
   return (
-    <div>
-      <button onClick={onLogInClick}>Logg inn</button>
-    </div>
+    <>
+      <button onClick={onRequestClick}>Send forespørsel</button>
+      <div>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </div>
+    </>
   );
 };
 
-export default Home;
+export default Main;
