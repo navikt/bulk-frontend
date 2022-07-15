a2b62fde3e6968a2de1c29e81e20ef76ce85e3f2
ahmeds99 <ahmed99@live.no>
2022-07-15 09:36:15 +0200

    feat: middleware for setting auth cookie, http-only
    Co-authored-by: ulrik2204 <ulrik@rosby@no>
    Co-authored-by: sigbbe <sigbjorn.berdal14@gmail.com>

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,18 @@
-// middleware.ts
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-// import { getSession } from 'next-auth/react'
 
 export function middleware(request: NextRequest) {
   const response = NextResponse.next();
+  if (request.nextUrl.pathname.includes("/_next")) {
+    return response;
+  }
+
   const cookie: string | null = request.headers.get("authorization");
   if (cookie) {
-    response.cookies.set("AuthorizationCookie", cookie);
+    response.cookies.set("AuthorizationCookie", cookie, {
+      httpOnly: true,
+    });
   }
+
   return response;
 }
