a763bfa076231bf47edb9d71f98fcdd5c5f7a329
ahmeds99 <ahmed99@live.no>
2022-07-21 10:33:51 +0200

    fix: add content-type
    Co-authored-by: ulrik2204 <ulrik@rosby@no>
    Co-authored-by: sigbbe <sigbjorn.berdal14@gmail.com>

--- src/pages/api/v1/personer.ts
+++ src/pages/api/v1/personer.ts
@@ -4,22 +4,23 @@ import { BACKEND_URL_PROXY } from "../../../lib/constants";
 const catchAll = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.url === undefined) return res;
 
-  const path = new URL(req.url).pathname.split("/").splice(3).join("/");
+  const path = req.url.split("/").splice(3).join("/");
   const token = req.headers.authorization ?? "";
   const { type } = req.query;
   const typeUsed = type ?? "json";
 
-  const personerResponse = (
-    await fetch(`${BACKEND_URL_PROXY}/${path}?type=${typeUsed}`, {
-      method: "POST",
-      headers: {
-        Authorization: token,
-      },
-      body: req.body,
-    }).then((res) => res.blob())
-  ).text();
+  const personerResponse = await fetch(`${BACKEND_URL_PROXY}/${path}`, {
+    method: "POST",
+    headers: {
+      Authorization: token,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req.body),
+  });
+  const data = await (await personerResponse.blob()).text();
+  res.setHeader("Content-type", typeUsed === "csv" ? "text/plain" : "application/json");
 
-  return res.status(200).send(personerResponse);
+  return res.status(personerResponse.status).send(data);
 };
 
 export default catchAll;
