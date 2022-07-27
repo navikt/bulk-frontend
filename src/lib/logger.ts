fb1c58c51bc1ed033820921e152de6374349de10
Ulrik Røsby <ulrik@rosby.no>
2022-07-27 16:04:54 +0200

    fix: logging in elasticsearch ecs format
    Co-authored-by: ahmeds99 <ahmed99@live.no>
    Co-authored-by: sigbbe <sigbjorn.berdal14@gmail.com>

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,5 +1,7 @@
+import ecsFormat from "@elastic/ecs-pino-format";
 import log from "pino";
 
-const logger = log();
+const logger = log(ecsFormat());
+logger.info("Initialized logger");
 
 export default logger;
